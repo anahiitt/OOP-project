a7bc91db5b9e77d2159f3e53920ac1ca190e65c7
Gayane <162709818+gayanebaghdasaryan3@users.noreply.github.com>
2024-04-13 23:52:24 +0400

    Add files via upload

---
+++ NoSuchTargetException.java
@@ -0,0 +1,8 @@
+public class NoSuchTargetException extends Exception{
+    public NoSuchTargetException(){
+        super();
+    }
+    public NoSuchTargetException(String message){
+        super(message);
+    }
+}
