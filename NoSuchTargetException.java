777e830593fea425443d7225f49bbea0ca699a59
anahiitt <143390595+anahiitt@users.noreply.github.com>
2024-05-10 23:41:48 +0400

    Delete NoSuchTargetException.java

--- NoSuchTargetException.java
+++
@@ -1,8 +0,0 @@
-public class NoSuchTargetException extends Exception{
-    public NoSuchTargetException(){
-        super();
-    }
-    public NoSuchTargetException(String message){
-        super(message);
-    }
-}
