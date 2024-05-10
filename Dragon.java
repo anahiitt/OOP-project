afd9b1262c9fb198c8bc77b559b73863fffd45b0
anahiitt <143390595+anahiitt@users.noreply.github.com>
2024-05-10 23:45:39 +0400

    Update Dragon.java

--- Dragon.java
+++ Dragon.java
@@ -1,11 +1,17 @@
 public class Dragon extends Monster{
     private int fireDamage;
 
-    public Dragon(String name, int hitPoints, int armorClass, int fireDamage){
-        super(name, hitPoints, armorClass);
+    public Dragon(String name, int hitPoints, int attackDamage, int armorClass, int fireDamage, boolean isALive, int x, int y){
+        super(name, hitPoints, attackDamage, armorClass, isALive, x, y);
         this.fireDamage = fireDamage;
     }
-
+    public Dragon(int x, int y){
+        super("Dragon", 15, 7, 19, true, x, y);
+        this.fireDamage = 8;
+    }
+    public String toString(){
+        return "D";
+    }
     public int getFireDamage(){
         return fireDamage;
     }
@@ -14,8 +20,9 @@ public class Dragon extends Monster{
         this.fireDamage = fireDamage;
     }
 
-    public void attack(){
-        System.out.println(name + " breathes fire, dealing " + fireDamage + " damage");
+    public void attackWithFire(Player target){
+        System.out.println(this.getName() + " breathes fire, hitting " + fireDamage + " damage");
+        attack(target);
     }
 
 }
